3d5201b094616a1735baa00981b826139100702e
Marcel Armada-Castellon <marcel.armada@armadacore.com>
2024-10-03 23:04:05 +0200

    refactor: :truck: rename variable

--- src/core/tokenizer/utils/declaration.rs
+++ src/core/tokenizer/utils/declaration.rs
@@ -1,3 +1,4 @@
+use crate::bin::constants;
 use crate::core::tokenizer::reader::{TokenReaderSnapshot, TokenReaderStack};
 use std::fmt::Debug;
 
@@ -8,13 +9,113 @@ pub fn structure_validation<T: Debug + Clone, F>(
 where
     F: FnMut(TokenReaderSnapshot) -> bool,
 {
-    while let Some(import_next_literal) = &stack.next_literal() {
+    while let Some(snapshot) = &stack.next_literal() {
         for declaration_item in structure.iter_mut() {
-            if declaration_item(import_next_literal.clone()) {
+            if declaration_item(snapshot.clone()) {
                 return true;
             }
         }
     }
 
     false
+}
+
+pub fn expected_token<F>(token: String, mut callback: F) -> impl FnMut(TokenReaderSnapshot) -> bool
+where
+    F: FnMut(TokenReaderSnapshot),
+{
+    let mut can_skip = false;
+
+    move |snapshot| {
+        if can_skip {
+            return false;
+        }
+
+        if let Some(snapshot_token) = &snapshot.token {
+            if snapshot_token.eq(&token) {
+                can_skip = true;
+                callback(snapshot);
+            }
+        }
+
+        can_skip
+    }
+}
+
+pub fn between_curly_braces<F>(mut callback: F) -> impl FnMut(TokenReaderSnapshot) -> bool
+where
+    F: FnMut(TokenReaderSnapshot),
+{
+    let mut can_skip = false;
+    let mut start_curly_braces = false;
+    let mut end_curly_braces = false;
+
+    move |snapshot| {
+        if can_skip {
+            return false;
+        }
+
+        if let Some(staring_curly_braces) = &snapshot.prev_token {
+            if staring_curly_braces == constants::START_CURLY_BRACES_TOKEN {
+                start_curly_braces = true;
+            }
+        }
+
+        if let Some(ending_curly_braces) = &snapshot.prev_token {
+            if ending_curly_braces == constants::END_CURLY_BRACES_TOKEN {
+                end_curly_braces = true;
+            }
+        }
+
+        if start_curly_braces && end_curly_braces {
+            can_skip = true;
+            start_curly_braces = false;
+            end_curly_braces = false;
+        }
+
+        if start_curly_braces {
+            callback(snapshot);
+        }
+
+        can_skip
+    }
+}
+
+pub fn between_single_quotes<F>(mut callback: F) -> impl FnMut(TokenReaderSnapshot) -> bool
+where
+    F: FnMut(TokenReaderSnapshot),
+{
+    let mut can_skip = false;
+    let mut start_single_quote = false;
+    let mut end_single_quote = false;
+
+    move |snapshot| {
+        if can_skip {
+            return false;
+        }
+
+        if let Some(starting_single_quotes) = &snapshot.prev_token {
+            if starting_single_quotes == constants::SINGLE_QUOTES_TOKEN {
+                if start_single_quote && !end_single_quote {
+                    end_single_quote = true;
+                }
+
+                if !start_single_quote {
+                    start_single_quote = true;
+                }
+            }
+        }
+
+        if start_single_quote && end_single_quote {
+            can_skip = true;
+            start_single_quote = false;
+            end_single_quote = false;
+        }
+
+        if start_single_quote {
+            callback(snapshot.clone());
+        }
+
+        can_skip
+    }
 }
