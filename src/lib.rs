05e8bee7d05edd25858d17971cf9e8e7069bc935
Marcel Armada-Castellon <marcel.armada@armadacore.com>
2024-10-05 15:29:13 +0200

    refactor: :recycle: remove unnecessary code

--- src/lib.rs
+++ src/lib.rs
@@ -11,17 +11,11 @@ pub mod bin;
 mod core;
 
 pub fn tokenize_source(path: &str) -> Result<Vec<TokenDeclaration>, ErrorCause> {
-    let file_path = exists_file(path)?;
-    let tokens = tokenizer::reader::new(file_path)?;
-    
-    Ok(tokens)
-}
-
-fn exists_file(path: &str) -> Result<PathBuf, ErrorCause> {
     let file_path = PathBuf::from(path);
-    
+
     if file_path.exists() && file_path.is_file() {
-        Ok(file_path)
+        let tokens = tokenizer::reader::new(file_path)?;
+        Ok(tokens)
     } else {
         Err(ErrorCause::FileNotFound(path))
     }
