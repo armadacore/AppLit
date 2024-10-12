9dd1c7bcafca3899164762f7d266568d61d85b28
Marcel Armada-Castellon <marcel.armada@armadacore.com>
2024-10-12 11:33:46 +0200

    refactor: :truck: move constant

--- src/bin/constants.rs
+++ src/bin/constants.rs
@@ -1,3 +1,5 @@
+use crate::bin;
+
 pub const MAIN_APP_FILE: &str = "main.app";
 
 pub const MAIN_APPLIT_FILE: &str = "main.applit";
@@ -25,3 +27,13 @@ pub const ARGUMENT_CLOSE: &str = ")";
 pub const KEYWORD_IMPORT: &str = "import";
 
 pub const KEYWORD_FROM: &str = "from";
+
+pub const REGEX_TOKENS_CONDITION: [&str; 7] = [
+    ARGUMENT_OPEN,
+    BLOCK_OPEN,
+    STATEMENT_ASSIGNMENT,
+    STATEMENT_DIVIDER,
+    BLOCK_CLOSE,
+    ARGUMENT_CLOSE,
+    STATEMENT_END,
+];
--- src/core/tokenizer/utils/string_utils.rs
+++ src/core/tokenizer/utils/string_utils.rs
@@ -1,18 +1,8 @@
-use crate::bin;
+use crate::bin::constants;
 use regex::Regex;
 
-const REGEX_TOKENS: [&str; 7] = [
-    bin::constants::ARGUMENT_OPEN,
-    bin::constants::BLOCK_OPEN,
-    bin::constants::STATEMENT_ASSIGNMENT,
-    bin::constants::STATEMENT_DIVIDER,
-    bin::constants::BLOCK_CLOSE,
-    bin::constants::ARGUMENT_CLOSE,
-    bin::constants::STATEMENT_END,
-];
-
 pub fn split_line(line: &str) -> Vec<String> {
-    let regex_tokens = REGEX_TOKENS.join("");
+    let regex_tokens = constants::REGEX_TOKENS_CONDITION.join("");
     let regex_pattern = format!(r#"'(?:\\'|[^'])*'|\w+|[{}]|\s"#, regex_tokens);
     let regexp = Regex::new(&regex_pattern).unwrap();
     let result: Vec<String> = regexp
