a87a548781d74e6a11bae1bdc325ac81f172cc63
Marcel Armada-Castellon <marcel.armada@armadacore.com>
2024-11-01 12:39:01 +0100

    feat: :sparkles: add scripts to generate project diagram for macOS
    Introduced project-diagram-macos.sh and all-diagram-macos.sh to automate creation and viewing of Rust project dependency diagrams. Updated .gitignore to exclude .doc directory.

---
+++ .scripts/project-diagram-macos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PARENT_DIR="$(dirname "$SCRIPT_DIR")"
+DOC_DIR="$PARENT_DIR/.doc"
+DOT_NAME="project-diagram.dot"
+IMG_NAME="project-diagram.svg"
+
+mkdir -p "$DOC_DIR"
+
+cargo modules dependencies --lib --no-externs --no-fns --no-sysroot --no-traits --no-types --no-uses > "$DOC_DIR/$DOT_NAME"
+
+if [ -f "$DOC_DIR/$DOT_NAME" ]; then
+       dot -Tsvg "$DOC_DIR/$DOT_NAME" -o "$DOC_DIR/$IMG_NAME"
+	   
+	   if [ -f "$DOC_DIR/$IMG_NAME" ]; then
+           open -a Safari "$DOC_DIR/$IMG_NAME"
+       else
+           echo "Error: $IMG_NAME wurde nicht erstellt."
+           exit 1
+       fi
+   else
+       echo "Error: $DOT_NAME is missing."
+       exit 1
+   fi
